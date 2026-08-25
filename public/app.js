32032a7ddcf7ef579dc7897ea464ca4a43910ebe
Sa1xp <142943429+New25Said@users.noreply.github.com>
2026-08-24 19:53:56 -0500

    Update VAPID key for Firebase messaging

--- public/app.js
+++ public/app.js
@@ -1225,7 +1225,7 @@ const initAppAfterLogin = async () => {
         const permission = await Notification.requestPermission();
         if (permission === 'granted' && swRegistration) {
             const token = await getToken(messaging, { 
-                vapidKey: "AQUI_TU_VAPID_KEY_DE_FIREBASE", // <-- ¡PON TU VAPID KEY AQUÍ!
+                vapidKey: "BNSVK5j4QOhpaqQgvkExMKvrq4Bwc50deeuIc3brEZyWlw9xSiC7sl0zoA3iSnWrj-6ImDL8pWkz_S0G-t0wj58", // <-- ¡PON TU VAPID KEY AQUÍ!
                 serviceWorkerRegistration: swRegistration 
             });
             if (token) {
